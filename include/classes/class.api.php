941f37004cdaabbc7c60d4f388a61dd0ca26159d
eze09 <emereuwaonueze@gmail.com>
2018-10-29 13:08:26 +0100

    Edit: Remove the function to fund rave wallet from class.api.php This is not the focus of this project

--- include/classes/class.api.php
+++ include/classes/class.api.php
@@ -1,4 +1,6 @@
 <?php
+
+
 //class for login section
 
 
@@ -13,43 +15,43 @@ class api extends general
 	//fund wallet
 	public function fundWallet($newamount)
 	{
-		$customer_email = "emereuwaonueze@gmail.com";//gets user email from db...but for the sake of this test....we are using my email
-		$amount = $newamount;  
-		$currency = "NGN";
-		$txref = "rave-".time(); // ensure you generate unique references per transaction.
-		$secretKey = $_ENV['SECRET_KEY'];
-		$env = $_ENV['ENV'];
-		$PBFPubKey = $_ENV['PUBLIC_KEY']; // we are suppose to pull from a table in our db name eg: "api-key". dont paste ur keys like this. but for the sake of this test, we will be doing the paste. 
-		$redirect_url = "http://localhost/demo/simplepayroll/dashboard/confirm-transaction.php";
-		$payment_plan = ""; // this is only required for recurring payments.
+		// $customer_email = "emereuwaonueze@gmail.com";//gets user email from db...but for the sake of this test....we are using my email
+		// $amount = $newamount;  
+		// $currency = "NGN";
+		// $txref = "rave-".time(); // ensure you generate unique references per transaction.
+		// $secretKey = $_ENV['SECRET_KEY'];
+		// $env = $_ENV['ENV'];
+		// $PBFPubKey = $_ENV['PUBLIC_KEY']; // we are suppose to pull from a table in our db name eg: "api-key". dont paste ur keys like this. but for the sake of this test, we will be doing the paste. 
+		// $redirect_url = "http://localhost/demo/simplepayroll/dashboard/confirm-transaction.php";
+		// $payment_plan = ""; // this is only required for recurring payments.
 
-		$array_options = array(
-			'amount'=>$amount,
-			'customer_email'=>$customer_email,
-			'currency'=>$currency,
-			'txref'=>$txref,
-			'PBFPubKey'=>$PBFPubKey,
-			'redirect_url'=>$redirect_url,
-			'payment_plan'=>$payment_plan
-		);
+		// $array_options = array(
+		// 	'amount'=>$amount,
+		// 	'customer_email'=>$customer_email,
+		// 	'currency'=>$currency,
+		// 	'txref'=>$txref,
+		// 	'PBFPubKey'=>$PBFPubKey,
+		// 	'redirect_url'=>$redirect_url,
+		// 	'payment_plan'=>$payment_plan
+		// );
 
-		$pay = new Pay();
-		$result = $pay->pay($PBFPubKey, $secretKey,$env,$array_options);
+		// $pay = new Pay();
+		// $result = $pay->pay($PBFPubKey, $secretKey,$env,$array_options);
 
-		$transaction = json_decode($result);
+		// $transaction = json_decode($result);
 
-		if(!$transaction->data && !$transaction->data->link){
-			// there was an error from the API
-			print_r('API returned error: ' . $transaction->message);
-		}
+		// if(!$transaction->data && !$transaction->data->link){
+		// 	// there was an error from the API
+		// 	print_r('API returned error: ' . $transaction->message);
+		// }
 		
-		// uncomment out this line if you want to redirect the user to the payment page
-		//print_r($transaction->data->message);
+		// // uncomment out this line if you want to redirect the user to the payment page
+		// //print_r($transaction->data->message);
 		
 		
-		// redirect to page so User can pay
-		// uncomment this line to allow the user redirect to the payment page
-		header('Location: ' . $transaction->data->link);
+		// // redirect to page so User can pay
+		// // uncomment this line to allow the user redirect to the payment page
+		// header('Location: ' . $transaction->data->link);
 	}
 
 	//get supported banks
